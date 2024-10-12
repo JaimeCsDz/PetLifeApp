26a67d9719aadee27768a5c57d74f9cfc5170050
Jaime <jaimecastaneda133@gmail.com>
2024-10-12 14:53:45 -0500

    Vista Inicio y vista login

--- src/ui/screens/auth/SignInScreen.tsx
+++ src/ui/screens/auth/SignInScreen.tsx
@@ -1,11 +1,145 @@
-import { ScrollView } from "react-native-gesture-handler"
+import { StackScreenProps } from "@react-navigation/stack";
+import { ScrollView } from "react-native-gesture-handler";
+import { RootStackParams } from "../../routes/StackNavigator";
+import { Text, TextInput, Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, StyleSheet, TouchableOpacity, Image, KeyboardAvoidingView, Platform } from 'react-native';
+import { Icon } from 'react-native-paper';
 
-export const SignInScreen = () => {
-    return(
-        <>
-        <ScrollView>
-            
-        </ScrollView>
-        </>
-    )
-}
+interface Props extends StackScreenProps<RootStackParams, "SignInScreen"> { }
+
+export const SignInScreen = ({ navigation }: Props) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.keyboardAvoidingContainer}
+                keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0}
+            >
+                <ScrollView contentContainerStyle={styles.scrollContent}>
+                    {/* Imagen del perro */}
+                    <View style={styles.header}>
+                        <Image source={require('../../../assets/Login.png')} style={styles.image}></Image>
+                    </View>
+
+                    {/* Formulario de inicio de sesión */}
+                    <View style={styles.formContainer}>
+                        <Text style={styles.welcomeText}>Bienvenido <Icon source='paw' size={40} color='#905010' /></Text>
+
+                        <TextInput
+                            label="Correo electrónico"
+                            mode="outlined"
+                            outlineColor="#C4C4C4"
+                            activeOutlineColor="#C4C4C4"
+                            placeholder="Ex: abc@example.com"
+                            placeholderTextColor={"#C4C4C4"}
+                            left={<TextInput.Icon icon="email"/>}
+                            style={styles.input}
+                        />
+
+                        <TextInput
+                            label="Contraseña"
+                            mode="outlined"
+                            secureTextEntry
+                            placeholder="********"
+                            placeholderTextColor={"#C4C4C4"}
+                            outlineColor="#C4C4C4"
+                            activeOutlineColor="#C4C4C4"
+                            left={<TextInput.Icon icon="lock" />}
+                            style={styles.input}
+                            textColor="#C4C4C4"
+                        />
+
+                        <TouchableOpacity>
+                            <Text style={styles.forgotPasswordText}>¿Olvidaste tu contraseña?</Text>
+                        </TouchableOpacity>
+
+                        <Button mode="contained" style={styles.loginButton}>
+                            Iniciar sesión
+                        </Button>
+
+                        <View style={styles.registerContainer}>
+                            <Text>¿No tienes una cuenta? </Text>
+                            <TouchableOpacity>
+                                <Text style={styles.registerText}>Regístrate</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF",
+    },
+    keyboardAvoidingContainer: {
+        flex: 1,
+    },
+    scrollContent: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 40,
+    },
+    header: {
+        alignItems: "center",
+        marginBottom: -40,
+        zIndex: 1,
+    },
+    image: {
+        width: 330,
+        height: 330,
+        zIndex: 1,
+        marginTop: -165,
+    },
+    formContainer: {
+        backgroundColor: "#FFFF",
+        borderRadius: 30,
+        padding: 20,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+        width: '80%',
+        zIndex: 0,
+        alignItems: "center", 
+    },
+    welcomeText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#037972",
+        marginBottom: 20,
+    },
+    input: {
+        marginBottom: 15,
+        color: "#C4C4C4",
+        backgroundColor: "#FFFFFF",
+        width: '100%',
+    },
+    forgotPasswordText: {
+        color: "#2F76E1",
+        textAlign: "right",
+        marginBottom: 20,
+    },
+    loginButton: {
+        backgroundColor: "#004E49",
+        paddingVertical: 5,
+        borderRadius: 30,
+        width: '100%',
+    },
+    registerContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginTop: 20,
+    },
+    registerText: {
+        color: "#2F76E1",
+        fontWeight: "bold",
+    },
+});
--- src/ui/screens/inicio/InicioScreen.tsx
+++ src/ui/screens/inicio/InicioScreen.tsx
@@ -1,16 +1,90 @@
-import { View } from "react-native"
-import { SafeAreaView } from "react-native"
-import { ScrollView } from "react-native"
-import { Text } from "react-native-paper"
+import { ImageBackground, View, StyleSheet, TouchableOpacity, Image } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, Text } from "react-native-paper"
+import { Icon } from 'react-native-paper'
+import { SvgXml } from "react-native-svg";
+import { StackScreenProps } from "@react-navigation/stack";
+import { RootStackParams } from "../../routes/StackNavigator";
 
-export const InicioScreen = () => {
-    return(
-        <>
-        <SafeAreaView>
-            <ScrollView>
-                <Text>Hola Mundo</Text>
-            </ScrollView>
+
+interface Props extends StackScreenProps<RootStackParams, "InicioScreen"> { }
+
+export const InicioScreen = ({navigation}: Props) => {
+
+    const onSignIn = () => navigation.navigate("SignInScreen")
+
+    return (
+        <SafeAreaView style={styles.container}>
+        <ImageBackground
+            source={require('../../../assets/backgroundInicio.png')}
+            resizeMode="cover"
+            style={styles.image}
+        >
+            <View style={styles.overlay} />
+            <View style={styles.content}>
+                <View style={styles.topContent}>
+                <Image source={require('../../../assets/Icono.png')} style={styles.logo}></Image>
+                <Text style={styles.title}>PetLife <Icon source='paw' size={40} color='#905010' /></Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={onSignIn}><Button mode="contained" style={styles.button}>Iniciar Sesión</Button></TouchableOpacity>
+                    <TouchableOpacity><Button mode="outlined" style={[styles.button, styles.outlinedButton]} textColor="#00635D">Registrarse</Button></TouchableOpacity>
+                </View>
+            </View>
+        </ImageBackground>
         </SafeAreaView>
-        </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    topContent: {
+        alignItems: 'center',
+        marginTop: 80
+    },
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'white',
+        opacity: 0.5,
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 50
+    },
+    logo: {
+        marginTop: -50,
+        alignSelf: 'center',
+        width: 400,
+        height: 400
+    },
+    title: {
+        fontSize: 40,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    button: {
+        marginTop: 15,
+        width: 270,
+        height: 50,
+        justifyContent: 'center',
+        backgroundColor: '#004E49',
+    },
+    outlinedButton: {
+        borderColor: '#004E49',
+        backgroundColor: 'transparent',
+        
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        width: '100%',
+        paddingBottom: 30
+    },
+});
